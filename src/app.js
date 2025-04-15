a797bc5111e206dfc9023c6364df1801076d6818
yash007i <pyash2268@gmail.com>
2025-04-15 22:20:27 +0530

    feat:  Updated app.js to include middleware for JSON parsing, URL encoding, CORS, and cookie parsing. - Removed redundant middleware setup from index.js.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,21 @@
 import express from "express"
+import { urlencoded } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
-const app = express()
+const app = express();
+app.use(express.json());
+app.use(urlencoded({
+    extended: true
+}));
+app.use(cors({
+        origin:`localhost:${process.env.PORT}`,
+        methods:['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type', 'Authorization']
+      })
+);
 
+app.use(cookieParser());
 // Router import
 import healthCheckRouter from "./routes/healthcheck.routes.js"
 import userRouter from "../src/routes/user.routes.js"
--- src/index.js
+++ src/index.js
@@ -1,27 +1,11 @@
 import app from "./app.js";
 import dotenv from "dotenv";
-import express, { urlencoded } from "express";
-import cors from "cors";
-import cookieParser from "cookie-parser";
 import connectDB from "./db/index.js";
 
 dotenv.config({
     path : "./.env",
 });
 const PORT = process.env.PORT || 9090;
-app.use(express.json());
-app.use(urlencoded({
-    extended: true
-}));
-app.use(cors({
-        origin:`localhost:${PORT}`,
-        methods:['GET', 'POST', 'PUT', 'DELETE'],
-        allowedHeaders: ['Content-Type', 'Authorization']
-      })
-);
-
-app.use(cookieParser());
-
 connectDB()
     .then(() => {
         app.listen(PORT , () => {
