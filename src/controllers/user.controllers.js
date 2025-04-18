f0535512e684c13f3d5d533c5d9d6aa8ce6de34e
yash007i <pyash2268@gmail.com>
2025-04-18 23:01:29 +0530

    feat: user registration process with avatar upload and email verification

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -2,14 +2,22 @@ import { asyncHandler } from "../utils/asyncHandler.js"
 import { User } from "../models/user.models.js"
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js"
+import { uploadOnCloudinary } from "../utils/cloudinary.js"
+import { sendMail,
+        emailVerificationMailgenContent,
+        forgotPasswordMailgenContent } from "../utils/sendMail.js"
 const registerUser = asyncHandler(async (req, res) => {
-    // Step to follow
+    // Step to follow :
     // get the user data from frontend
     // validation - not empty --> done by validator & middleware 
     // check if user already exists : email, username
-    // create user object - create entry in DB  
-    // remove password and refresh token filed from response
+    // if avatar then upload in cloudinary 
+    // create user object - create entry in DB
     // check for user creation
+    // generate temp token for email verification useing generateTemporaryToken()
+    // store hashed token and expiry in db
+    // make verification url for email verification
+    // send verification mail
     // return response 
 
     // get the user data from frontend
@@ -24,21 +32,52 @@ const registerUser = asyncHandler(async (req, res) => {
         throw new ApiError(409, "User is alredy exist with this email.");
     }
     
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    const avatar = await uploadOnCloudinary(avatarLocalPath);
+
     const user = await User.create({
+        username: username.toLowerCase(),
         fullname,
         email,
         password,
-        username: username.toLowerCase()
+        avatar : {
+            url :avatar?.url || "",
+            localPath : avatarLocalPath || ""
+        }
     });
     
     if(!user){
         throw new ApiError(500, "Something went wrong, while creating a user.")
     }
 
-    const createdUser = user.select("-password -refreshToken");
+    const { hasedToken,tokenExpiry,unHasedToken } = user.generateTemporaryToken()
+    user.emailVerificationToken = hasedToken;
+    user.emailVerificationExpiry = tokenExpiry;
+    await user.save();
+
+    //finding user with username and email
+    const registerUser = await User.findOne(
+        { _id: user._id },
+        { username: 1, email: 1, fullname: 1 },
+    );
+
+    if (!registerUser) {
+        throw new ApiError(500, "Something went wrong while registering the user");
+    }
+
+    const verificationURL = `${process.env.BASEURL}/users/verify-email?token=${unHasedToken}`
+
+    const isSendEmail = await sendMail({
+        email : registerUser.email,
+        subject : "Verify your email address",
+        mailgenContent : emailVerificationMailgenContent (
+            registerUser.username,
+            verificationURL,
+        ),
+    });
 
     return res.status(201).json(
-        new ApiResponse(200, createdUser, "User rigister successfully")
+        new ApiResponse(200, registerUser, "User register successfully")
     )
 
 })
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,10 +2,24 @@ import { Router } from "express";
 import { registerUser } from "../controllers/user.controllers.js"
 import { userRegistrationValidator } from "../validators/user.validators.js";
 import { validate } from "../middlewares/validator.middlewares.js";
+import { upload } from "../middlewares/multer.middleware.js"
 
 const router = Router();
 
 router.route("/register")
+    .post(
+    upload.fields(
+        [
+            {
+                name : "avatar",
+                maxCount : 1
+            }
+        ]
+    ),
+    userRegistrationValidator(),
+    validate,
+    registerUser);
+router.route("/verify-email:token")
     .post(userRegistrationValidator(), validate, registerUser);
 
 export default router
